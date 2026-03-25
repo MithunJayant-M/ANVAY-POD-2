b95f54e847659e1823c484223e7e9d221f38bd7b
M MITHUN JAYANT <147626913+MithunJayant-M@users.noreply.github.com>
2026-03-25 09:06:34 +0530

    Add files via upload

---
+++ anvay/anvay/src/main/java/com/cts/mfrp/anvay/AnvayApplication.java
@@ -0,0 +1,13 @@
+package com.cts.mfrp.anvay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnvayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnvayApplication.class, args);
+	}
+
+}
---
+++ anvay/anvay/src/test/java/com/cts/mfrp/anvay/AnvayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.mfrp.anvay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnvayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
